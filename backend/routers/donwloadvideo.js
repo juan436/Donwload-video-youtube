d64a3de67b4d45935e61a7507a5706dba5f95163
Juan Villegas <juancvillefer@gmail.com>
2023-11-07 22:10:54 -0400

    add test postman and endpoint mp3 and mp4

---
+++ backend/routers/donwloadvideo.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+
+const routerDonwloadVideo = express.Router();
+routerDonwloadVideo.use(express.json());
+
+routerDonwloadVideo.get('/formatomp4', async (req, res) => {
+
+    try {
+        var url = req.query.url;
+        if (!ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+
+        // Información basica del video
+        let title = 'video';
+        await ytdl.getBasicInfo(url, {
+            format: 'mp4'
+        }, (err, info) => {
+            title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+        });
+
+        res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+        ytdl(url, {
+            format: 'mp4'
+        }).pipe(res);
+
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+
+routerDonwloadVideo.get('/formatomp3', async (req, res) => {
+
+    try {
+        var url = req.query.url;
+        if (!ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+
+        // Información basica del video
+        let title = 'audio';
+        await ytdl.getBasicInfo(url, {
+            format: 'mp3'
+        }, (err, info) => {
+            title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+        });
+
+        res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+		ytdl(url, {
+			format: 'mp3',
+			filter: 'audioonly',
+		}).pipe(res);
+
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+module.exports = routerDonwloadVideo;
+
+
+
+
+
+
+
+
